b5556d75845e23a516af6015420732a16c30cfab
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-04-16 18:38:51 -0400

    completes rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,25 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    plays = ['rock', 'paper', 'scissors']
+    result = []
+
+    def helper(available_plays, rounds):
+        if rounds == 0:
+            result.append(available_plays)
+        else:
+            for i in range(len(plays)):
+                helper(available_plays+[plays[i]], rounds - 1)
+
+    helper([], n)
+    return result
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
